97adbe877941fb661c5bda512f5ed71ac70db558
Andrey <andreybr2002@hotmail.com>
2022-05-04 20:44:06 -0300

    Ajuestes no index

--- index.php
+++ index.php
@@ -5,18 +5,14 @@
     <meta charset="UTF-8">
     <link rel="icon" href="assets/icone_pagina.svg">
     <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="forma-pagamento.css">
-    <link rel="stylesheet" href="view-recibo.css">
-    <link rel="stylesheet" href="responsividade.css">
-    <link rel="stylesheet" href="labels.css">
     <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <title>Recibo de Prestação de Serviço</title>
+    <title>Vale</title>
 </head>
 
 <body>
     <div class="main" id="main">
         <form action="#" class="form" id="form-all" onsubmit="gerarRecibo();return false">
-            <h1 class="titulo">Recibo de Prestação de Serviço</h1>
+            <h1 class="titulo">Vale</h1>
 
             <div class="row">
                 <div class="column-numero-recibo">
@@ -30,9 +26,9 @@
                     <p class="help">Valor recebido</p>
                 </div>
             </div>
-            <div class="column-contratante">
+            <div class="column-nome">
                 <h4>Nome:</h4>
-                <input class="input-contratante" id="contratante" required type="text" placeholder="Ex.: Nome do funcionário">
+                <input class="input-nome" id="nome" required type="text" placeholder="Ex.: Nome do funcionário">
             </div>
             <div class="row">
                 <div class="column-cidade">
@@ -55,9 +51,9 @@
                         }
                     </script>
                 </div>
-                <div class="column-prestador-servico">
+                <div class="column-dados-empresa">
                     <h4>Dados da Empresa (opcional):</h4>
-                    <input class="input-prestador-servico" id="nome-ps" type="text">
+                    <input class="input-dados-empresa" id="nome-ps" type="text" placeholder="Ex.: Nome da sua empresa, CNPJ, Endereço...">
                 </div>
             </div>
             <div class="column-duas-vias">
@@ -76,6 +72,7 @@
                 </button>
             </div>
         </form>
+        <script src="mascaras-form.js"></script>
 </body>
 
 </html>
