1b38812a3d49f2b5e2e82486e17014849067b279
Andrey <andreybr2002@hotmail.com>
2022-05-04 22:06:26 -0300

    Iniciando implementação do php

--- index.php
+++ index.php
@@ -5,39 +5,39 @@
     <meta charset="UTF-8">
     <link rel="icon" href="assets/icone_pagina.svg">
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="responsividade.css">
     <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <title>Vale</title>
 </head>
 
 <body>
     <div class="main" id="main">
-        <form action="#" class="form" id="form-all" onsubmit="gerarRecibo();return false">
-            <h1 class="titulo">Vale</h1>
-
+        <form class="form" id="form-all" action="index.php" method="post">
+            <h1 class=" titulo">Vale</h1>
             <div class="row">
                 <div class="column-numero-recibo">
                     <h4 class="label-valor">Número (opcional):</h4>
-                    <input class="input-numero-recibo" id="numero-recibo" type="number">
+                    <input class="input-numero-recibo" id="numero-recibo" type="number" name="mumero-recio">
                     <p class="help">Número vale</p>
                 </div>
                 <div class="column-valor">
                     <h4 class="label-valor">Valor:</h4>
-                    <input class="input-valor" id="valor-recibo" type="text" required placeholder="00,00" name="Valor">
+                    <input class="input-valor" id="valor-recibo" type="text" required placeholder="00,00" name="valor">
                     <p class="help">Valor do vale</p>
                 </div>
             </div>
             <div class="column-nome">
                 <h4>Nome:</h4>
-                <input class="input-nome" id="nome" required type="text" placeholder="Ex.: Nome do funcionário">
+                <input class="input-nome" id="nome" required type="text" placeholder="Ex.: Nome do funcionário" name="nome">
             </div>
             <div class="row">
                 <div class="column-cidade">
                     <h4>Cidade:</h4>
-                    <input class="input-cidade" type="text" id="cidade" required>
+                    <input class="input-cidade" type="text" id="cidade" required name="cidade">
                 </div>
                 <div class="column-data">
                     <h4>Data:</h4>
-                    <input class="input-data" type="text" id="placeholderSysDate" required maxlength="10">
+                    <input class="input-data" type="text" id="placeholderSysDate" required maxlength="10" name="data">
                     <script>
                         date = new Date();
                         year = date.getFullYear();
@@ -53,7 +53,7 @@
                 </div>
                 <div class="column-dados-empresa">
                     <h4>Dados da Empresa (opcional):</h4>
-                    <input class="input-dados-empresa" id="nome-ps" type="text" placeholder="Ex.: Nome da sua empresa, CNPJ, Endereço...">
+                    <input class="input-dados-empresa" id="nome-ps" type="text" placeholder="Ex.: Nome da sua empresa, CNPJ, Endereço..." name="dados-empresa">
                 </div>
             </div>
             <div class="column-duas-vias">
@@ -72,6 +72,13 @@
                 </button>
             </div>
         </form>
+        <?php
+        echo "VALE:     Nº" . $_POST["mumero-recio"], "     R$ " . $_POST["valor"], "#
+        NOME: " . $_POST["nome"],
+        "VALOR: " . $_POST["valor"],
+        "LOCAL/DATA: " . $_POST["cidade"], ", "  . $_POST["data"], $_POST["dados-empresa"];
+        ?>
+
         <script src="mascaras-form.js"></script>
 </body>
 
