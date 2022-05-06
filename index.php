e7a3aba501b264f308db2a1eeb9f09e6ac6ccbc6
Andrey <andreybr2002@hotmail.com>
2022-05-06 00:19:39 -0300

    Finalizando emissor de Vale

--- index.php
+++ index.php
@@ -11,68 +11,68 @@
 </head>
 
 <body>
-    <div class="main" id="main">
-        <form class="form" id="form-all" action="recibo.php" method="post">
-            <h1 class=" titulo">Vale</h1>
-            <div class="row">
-                <div class="column-numero-recibo">
-                    <h4 class="label-valor">Número (opcional):</h4>
-                    <input class="input-numero-recibo" id="numero-recibo" type="number" name="mumero-recibo">
-                    <p class="help">Número vale</p>
-                </div>
-                <div class="column-valor">
-                    <h4 class="label-valor">Valor:</h4>
-                    <input class="input-valor" id="valor-recibo" type="text" required placeholder="00,00" name="valor">
-                    <p class="help">Valor do vale</p>
-                </div>
+<div class="main" id="main">
+    <form class="form" id="form-all" action="recibo.php" method="post">
+        <h1 class=" titulo">Vale</h1>
+        <div class="row">
+            <div class="column-numero-recibo">
+                <h4 class="label-valor">Número (opcional):</h4>
+                <input class="input-numero-recibo" id="numero-recibo" type="number" name="mumero-recibo">
+                <p class="help">Número vale</p>
             </div>
-            <div class="column-nome">
-                <h4>Nome:</h4>
-                <input class="input-nome" id="nome" required type="text" placeholder="Ex.: Nome do funcionário" name="nome">
+            <div class="column-valor">
+                <h4 class="label-valor">Valor:</h4>
+                <input class="input-valor" id="valor-recibo" type="text" required placeholder="00,00" name="valor">
+                <p class="help">Valor do vale</p>
             </div>
-            <div class="row">
-                <div class="column-cidade">
-                    <h4>Cidade:</h4>
-                    <input class="input-cidade" type="text" id="cidade" required name="cidade">
-                </div>
-                <div class="column-data">
-                    <h4>Data:</h4>
-                    <input class="input-data" type="text" id="placeholderSysDate" required maxlength="10" name="data">
-                    <script>
-                        date = new Date();
-                        year = date.getFullYear();
-                        month = date.getMonth() + 1;
-                        day = date.getDate();
+        </div>
+        <div class="column-nome">
+            <h4>Nome:</h4>
+            <input class="input-nome" id="nome" required type="text" placeholder="Ex.: Nome do funcionário" name="nome">
+        </div>
+        <div class="row">
+            <div class="column-cidade">
+                <h4>Cidade:</h4>
+                <input class="input-cidade" type="text" id="cidade" required name="cidade">
+            </div>
+            <div class="column-data">
+                <h4>Data:</h4>
+                <input class="input-data" type="text" id="placeholderSysDate" required maxlength="10" name="data">
+                <script>
+                    date = new Date();
+                    year = date.getFullYear();
+                    month = date.getMonth() + 1;
+                    day = date.getDate();
 
-                        if (month < 10 || date < 10) {
-                            document.getElementById("placeholderSysDate").value = '0' + day + '/0' + month + '/' + year;
-                        } else {
-                            document.getElementById("placeholderSysDate").value = day + '/' + month + '/' + year;
-                        }
-                    </script>
-                </div>
-                <div class="column-dados-empresa">
-                    <h4>Dados da Empresa (opcional):</h4>
-                    <input class="input-dados-empresa" id="nome-ps" type="text" placeholder="Ex.: Nome da sua empresa, CNPJ, Endereço..." name="dados-empresa">
-                </div>
+                    if (month < 10 || date < 10) {
+                        document.getElementById("placeholderSysDate").value = '0' + day + '/0' + month + '/' + year;
+                    } else {
+                        document.getElementById("placeholderSysDate").value = day + '/' + month + '/' + year;
+                    }
+                </script>
             </div>
-            <div class="column-duas-vias">
-                <h4 class="label-duas-vias">Duas vias?</h4>
-                <label class="checkbox">
-                    <input class="checkbox" type="checkbox" name="duasvias" id="duasvias">
-                    Sim
-                </label>
+            <div class="column-dados-empresa">
+                <h4>Dados da Empresa (opcional):</h4>
+                <input class="input-dados-empresa" id="nome-ps" type="text" placeholder="Ex.: Nome da sua empresa, CNPJ, Endereço..." name="dados-empresa">
             </div>
-            <div class="botao-gerar-recibo">
-                <button type="submit">
+        </div>
+        <div class="column-duas-vias">
+            <h4 class="label-duas-vias">Duas vias?</h4>
+            <label class="checkbox">
+                <input class="checkbox" type="checkbox" name="duasvias" id="duasvias">
+                Sim
+            </label>
+        </div>
+        <div class="botao-gerar-recibo">
+            <button type="submit">
                     <span class="material-symbols-outlined">
                         description
                     </span>
-                    Gerar Recibo
-                </button>
-            </div>
-        </form>
-        <script src="mascaras-form.js"></script>
+                Gerar Recibo
+            </button>
+        </div>
+    </form>
+    <script src="mascaras-form.js"></script>
 </body>
 
 </html>
--- recibo.php
+++ recibo.php
@@ -11,46 +11,58 @@
 </head>
 
 <body>
-    <div class="main" id="main">
-        <h1 class=" titulo">Vale</h1>
-        <div class="row">
-            <div class="column-vale">
-                <h4 class="label-valor">VALE</h4>
-            </div>
-            <div class="column-dados-vale">
-                <?php
-                echo "VALE:     Nº" . $_POST["mumero-recibo"], "  R$ " . $_POST["valor"], "#";
-                ?>
-            </div>
+<div class="main" id="main">
+    <h1 class=" titulo">Vale</h1>
+    <div class="row">
+        <div class="column-vale">
+            <h4 class="label-valor">VALE</h4>
         </div>
-        <div class="row">
-            <div class="column-nome">
-                <?php
-                echo "NOME: " . $_POST["nome"];
-                ?>
-            </div>
+        <div class="column-dados-vale">
+            <?php
+            echo "VALE:     Nº" . $_POST["mumero-recibo"], "  R$ " . $_POST["valor"], "#";
+            ?>
         </div>
-        <div class="row">
-            <div class="column-valor">
-                <?php
-                echo "VALOR: " . $_POST["valor"];
-                ?>
-            </div>
+    </div>
+    <div class="row">
+        <div class="column-nome">
+            <?php
+            echo "NOME: " . $_POST["nome"];
+            ?>
         </div>
-        <div class="row">
-            <div class="column-local-data">
-                <?php
-                echo "LOCAL/DATA: " . $_POST["data"];
-                ?>
-            </div>
+    </div>
+    <div class="row">
+        <div class="column-valor">
+            <?php
+            echo "VALOR: " . $_POST["valor"];;
+            ?>
         </div>
-        <div class="row">
-            <div class="column-dados-empresa">
-                <?php
-                echo $_POST["dados-empresa"];
-                ?>
-            </div>
+    </div>
+    <div class="row">
+        <div class="column-local-data">
+            <?php
+            echo "LOCAL/DATA: " . $_POST["cidade"], ", ", $_POST["data"];
+            ?>
         </div>
+    </div>
+    <div class="borda-assinatura">
+        <p class="assinatura-label">Assinatura</p>
+    </div>
+    <div class="row">
+        <div class="column-dados-empresa">
+            <?php
+            echo $_POST["dados-empresa"];
+            ?>
+        </div>
+    </div>
+    <div class="botao-gerar-recibo">
+        <button onclick="imprimir()">
+                <span class="material-symbols-outlined">
+                    description
+                </span>
+            Gerar Recibo
+        </button>
+    </div>
+    <script src="funcoes.js"></script>
 </body>
 
 </html>
